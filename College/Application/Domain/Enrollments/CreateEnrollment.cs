54365bbcf1629a99d6f8b8a99b6b836c24a7d866
Nicolas Prestes <maturemtabmm@gmail.com>
2023-03-03 18:58:28 -0300

    adicionado validação de curso existente.

--- College/Application/Domain/Enrollments/CreateEnrollment.cs
+++ College/Application/Domain/Enrollments/CreateEnrollment.cs
@@ -7,13 +7,21 @@ namespace College.Application.Domain.Enrollments
     public class CreateEnrollment
     {
         private readonly IEnrollmentRepository enrollmentRepository;
+        private readonly ICourseRepository courseRepository;
 
-        public CreateEnrollment(IEnrollmentRepository enrollmentRepository)
+        public CreateEnrollment(IEnrollmentRepository enrollmentRepository, ICourseRepository courseRepository)
         {
             this.enrollmentRepository = enrollmentRepository;
+            this.courseRepository = courseRepository;
         }
-        public async Task<Enrollment> Execute (EnrollmentDTO enrollmentDTO)
+        public async Task<Enrollment> Execute(EnrollmentDTO enrollmentDTO)
         {
+            var courseExist = await this.courseRepository.FindByIdAsync(enrollmentDTO.CourseId);
+            if (courseExist == null)
+            {
+                throw new Exception("Curso não encontrado");
+            }
+
             var enrollment = new Enrollment(
                     enrollmentDTO.StudentId,
                     enrollmentDTO.CourseId,
